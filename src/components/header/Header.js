dc9f455e75b991d5b4c557ba73c46a44da73891c
Akash Srivastava <akash221002@gmail.com>
2021-12-20 15:49:26 +0530

    Added styles to header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,40 @@
 import React from "react";
-// import "./header.css";
-
+import "./header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 export default function Header() {
   return (
     <div className="header">
       <img
-        className="header_logo"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo9E3tKDk0sz2KCHW7m9r909IaMFHYFzYZsw&usqp=CAU"
+        className="header__logo"
+        src="https://www.seekpng.com/png/detail/428-4289671_logo-e-commerce-good-e-commerce-logo.png"
         alt=""
       />
+
       <div className="header__search">
-        <input className="header_searchInput" type="text" />
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
+
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
           <span className="header__optionLineTwo">Sign In</span>
         </div>
+
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo"> & Orders</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
+
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
