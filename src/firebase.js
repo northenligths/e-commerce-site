41967580a6b3f76cabe7a9972bbfb832b2248f12
Akash Srivastava <akash221002@gmail.com>
2021-12-20 14:43:35 +0530

    Added firebase to the project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbsRq6pq30KIXV0c88RCVKPio3xHEOTZ4",
+  authDomain: "e-commerce-7a6a3.firebaseapp.com",
+  projectId: "e-commerce-7a6a3",
+  storageBucket: "e-commerce-7a6a3.appspot.com",
+  messagingSenderId: "535217759179",
+  appId: "1:535217759179:web:7a2fe0c3722c4453bacc4e",
+};
