c571259dfc68ddffb481ed1de7e783f8b8ce2bc9
Akash Srivastava <akash221002@gmail.com>
2021-12-20 15:05:40 +0530

    Added input field and options in header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,7 +9,23 @@ export default function Header() {
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo9E3tKDk0sz2KCHW7m9r909IaMFHYFzYZsw&usqp=CAU"
         alt=""
       />
-      <div className="header__search"></div>
+      <div className="header__search">
+        <input className="header_searchInput" type="text" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo"> & Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+      </div>
     </div>
   );
 }
