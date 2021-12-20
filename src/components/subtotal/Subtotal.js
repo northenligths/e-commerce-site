d00baa4916e4c6185564a09755214d3ee867b9b0
Akash Srivastava <akash221002@gmail.com>
2021-12-20 19:53:26 +0530

    Created subtotal component and rendered it in checkout page

--- src/components/subtotal/Subtotal.js
+++ src/components/subtotal/Subtotal.js
@@ -1,7 +1,30 @@
 import React from "react";
 import "./subtotal.css";
+import CurrencyFormat from "react-currency-format"; //package to display money
 function Subtotal() {
-  return <div className="subtotal"></div>;
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        //rendertext is what is actually shown to the screen
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items):<strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2} //display money in two decimal places
+        value={0} //this displays the actual amount
+        displayType={"text"}
+        thousandSeparator={true} //separates money in thousand by commas and display them
+        prefix={"$"}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 }
 
 export default Subtotal;
