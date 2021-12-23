fe1bdbe33a83077553596159a286078b63f2c4c9
Akash Srivastava <akash221002@gmail.com>
2021-12-23 15:26:46 +0530

    Created Login page and added styles to it

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Checkout from "./components/checkout/Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
